fb62b347c04ea8fc6fddaed2cdc9070d76a6afdf
unknown <464319680@qq.com>
2021-10-28 07:10:44 +0800

    HWFinal002

---
+++ CalculateTwoIntegers/MainFunction.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Calculation Module.h>
+#include <Boost Module.h>
+#include <EarlyDemo.h>
+
+int main()
+{
+    Welcome();
+    int WorkingMode = 0;
+    ChooseWorkingMode(&WorkingMode);
+    switch(WorkingMode)
+    {
+        case 1:WindowsStart(); break;
+        case 2:FileStart(); break;
+        default:printf("%s", "启动失败\n");
+    }
+    return 0;
+}
